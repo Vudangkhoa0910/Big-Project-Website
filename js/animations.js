c2de36729931765b1ef87c4884293e8b596df91e
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-05-26 18:23:24 +0700

    Push update animations.js 1t

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,4 @@
 const text = "PHENIKAA TRAVEL";
-
 const animatedText = document.getElementById('animated-text');
 
 function typeWriter(text, i, isFirstTime) {
@@ -8,8 +7,16 @@ function typeWriter(text, i, isFirstTime) {
   }
   if (i < text.length) {
     const charElement = document.createElement('span');
+
+    if (i < 8) { 
+      charElement.className = 'phenikaa';
+    } else if (i >= 9) { 
+      charElement.className = 'travel';
+    }
+
     charElement.textContent = text.charAt(i);
     animatedText.appendChild(charElement);
+
     setTimeout(function() {
       if (text.charAt(i) === ' ') {
         animatedText.innerHTML += ' ';
@@ -17,14 +24,16 @@ function typeWriter(text, i, isFirstTime) {
       typeWriter(text, i + 1, false);
     }, 100);
   } else {
+    const logoElement = document.createElement('span');
+    logoElement.className = 'logo-behind'; 
+    logoElement.innerHTML = '&nbsp;<img src="./images/Home/logomain.png" alt="Logo Behind Text" class="logo-image">'; // Thêm một khoảng trống trước khi chèn hình ảnh logo
+    animatedText.appendChild(logoElement);
+
     setTimeout(function() {
       animatedText.innerHTML = ''; 
-      typeWriter(text, 0, false); 
+      typeWriter(text, 0, true); 
     }, 2000); 
   }
 }
 
 typeWriter(text, 0, true);
-
-
-
