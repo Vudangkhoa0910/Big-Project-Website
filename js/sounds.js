f50909afae7888a0fecd681378cbeed7ab1644ad
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-06-08 07:07:02 +0700

    Push update audio/js 3t

--- js/sounds.js
+++ js/sounds.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var audio = document.getElementById('background-audio');
   var muteButton = document.getElementById('mute-button');
   var muteImage = muteButton.querySelector('img'); 
-  var muteSrc = 'images/Home/mute.png';
-  var unmuteSrc = 'images/Home/unmute.png';
-  audio.volume = 0.5; // Set volume to 50%
+  var muteSrc = './images/Home/mute.png';
+  var unmuteSrc = './images/Home/unmute.png';
+  audio.volume = 0.1; 
 
   muteButton.addEventListener('click', function() {
     if (audio.muted) {
