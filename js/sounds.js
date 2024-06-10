71d0b57600a0de066252a7a1a5052848d270b4b9
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-06-10 08:51:39 +0700

    Push update sounds/js 2t

--- js/sounds.js
+++ js/sounds.js
@@ -1,19 +1,29 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  var audio = document.getElementById('background-audio');
-  var muteButton = document.getElementById('mute-button');
-  var muteImage = muteButton.querySelector('img'); 
-  var muteSrc = './images/Home/mute.png';
-  var unmuteSrc = './images/Home/unmute.png';
-  audio.volume = 0.1; 
+    var audio = document.getElementById('background-audio');
+    var muteButton = document.getElementById('mute-button');
+    var muteImage = muteButton.querySelector('img');
+    var muteSrc = './images/Home/mute.png';
+    var unmuteSrc = './images/Home/unmute.png';
 
-  muteButton.addEventListener('click', function() {
-    if (audio.muted) {
-      audio.muted = false;
-      muteImage.src = unmuteSrc; 
-    } else { 
-      audio.muted = true;
-      muteImage.src = muteSrc; 
+    audio.volume = 0.1;
+
+    function playAudio() {
+        audio.play().then(function() {
+            console.log('Audio is playing automatically.');
+        }).catch(function(error) {
+            console.log('Autoplay was prevented:', error);
+        });
     }
-  });
+
+    playAudio();
+
+    muteButton.addEventListener('click', function() {
+        if (audio.muted) {
+            audio.muted = false;
+            muteImage.src = unmuteSrc;
+        } else {
+            audio.muted = true;
+            muteImage.src = muteSrc;
+        }
+    });
 });
